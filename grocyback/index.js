b4fcaaff84d77d69cdf05e88be96d7f7f02b970c
AumThaker <thakeraum1511@gmail.com>
2025-02-18 13:40:01 +0530

    commit

--- grocyback/index.js
+++ grocyback/index.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOption = {
-    origin:['http://localhost:3001','https://grocify-now.vercel.app/login'],
+    origin:['http://localhost:3001','https://grocify-now.vercel.app/'],
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Accept']
